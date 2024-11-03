7e9d9fcc603933dbc52a1f1c77c6f42db61a8fd0
OranLevy1 <oranlevy9@gmail.com>
2024-11-03 10:59:48 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,13 @@ public class Main {
 	     }
 		return arr[i] + ex14(arr, i - 1);
 	}
-	public static int ex15 (int[] arr, int i) {
-		if (i==0) {
-			return arr[0];
+	public static int ex15(int [] arr, int i) {
+		if (i==-1)
+			return 0;
+		else if (arr[i]<=0) {
+			return ex15(arr,i-1);
 		}
-		if (arr[i]>0)
-			return arr[i]+ ex15(arr,i-1);
-		return ex15(arr,i-1);
+		return 1+ex15(arr,i-1);
 	}
 	public static int ex16(int[] arr, int n,int i) {
 		if (i==0) {
